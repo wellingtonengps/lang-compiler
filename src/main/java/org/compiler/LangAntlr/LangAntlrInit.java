eb697842c504799911b36ffa265bd842cda6e406
Wellington Pereira <43380060+wellingtonengps@users.noreply.github.com>
2024-04-07 17:26:17 -0300

    feat: pegando o arquivo por argumento

--- src/main/java/org/compiler/LangAntlr/LangAntlrInit.java
+++ src/main/java/org/compiler/LangAntlr/LangAntlrInit.java
@@ -3,11 +3,13 @@ package org.compiler.LangAntlr;
 import org.antlr.v4.runtime.*;
 import org.antlr.v4.runtime.tree.*;
 
+import java.io.FileReader;
+
 public class LangAntlrInit {
     public static void main(String[] args)
             throws Exception {
 
-        ANTLRInputStream input = new ANTLRInputStream(System.in);
+        ANTLRInputStream input = new ANTLRInputStream(new FileReader(args[0]));
 
         //LangAntlrInitLexer lexer = new LangAntlrInitLexer(input);
 
