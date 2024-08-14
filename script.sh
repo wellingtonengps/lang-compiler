a64fa5d95d5e0a031f60a275b6801f67a741e647
Wellington Pereira <43380060+wellingtonengps@users.noreply.github.com>
2024-08-13 21:33:48 -0300

    fix script.sh

--- script.sh
+++ script.sh
@@ -2,5 +2,5 @@ echo "Build project..."
 mvn clean package -DskipTests
 
 echo "Running JAR..."
-java -cp target/lang-compiler-1-jar-with-dependencies.jar org.compiler.Lang.LangInit "$1"
+java -cp target/lang-compiler-1-jar-with-dependencies.jar org.compiler.Lang.LangInit $1
 
